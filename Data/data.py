1e6a636b9ce400b37c1e8bfad6401c8226063961
웅 장 <ahfrhgkwl@naver.com>
2024-08-19 19:34:49 +0900

    과거 사정율 날짜 기반 표기 변경

--- Data/data.py
+++ Data/data.py
@@ -19,14 +19,27 @@ def GetBidResultsByBidNo(bidNo, isPrint=False):
   Parameters:
   - bidNo(int): 공고번호
   Returns:
-  - (id,공고번호,업체명,대표명,사업자번호,등수,입찰액,투찰율,사정율)
+  - (id,공고번호,업체명,대표명,사업자번호,등수,입찰액,투찰율,사정율,예가범위,공고일자)
   """
   conn = GetConnection()
   if conn == 0:
     return
   cur = conn.cursor()
 
-  query = f"SELECT * FROM bidresults WHERE bidno = '{bidNo}'"
+  query = f"""
+    SELECT 
+        br.*,
+        b.pricerange,
+        b.biddate
+    FROM 
+        bidresults br
+    JOIN 
+        bids b
+    ON 
+        br.bidno = b.bidno
+    WHERE 
+        br.bidno = '{bidNo}';
+    """
   cur.execute(query)
   result = cur.fetchall()
  
@@ -36,19 +49,53 @@ def GetBidResultsByBidNo(bidNo, isPrint=False):
     print(result)
   return result
 
-def GetBizResultsByBizNo(bizNo, isPrint=False):
+def GetBidsByBidNo(bidNo, isPrint=False):
   """
   Parameters:
-  - bizNo(int): 사업자번호
+  - bidNo(int): 공고번호
   Returns:
-  - (id,공고번호,업체명,대표명,사업자번호,등수,입찰액,투찰율,사정율)
+  - (공고번호,순공사원가사용여부,입찰하한가,기초가격,A값,자격요건,공고차수,취소여부,예가범위,순공사원가액수,공고일자)
   """
   conn = GetConnection()
   if conn == 0:
     return
   cur = conn.cursor()
 
-  query = f"SELECT * FROM bidresults WHERE bizno = '{bizNo}'"
+  query = f"SELECT * FROM bids WHERE bidno = '{bidNo}'"
+  cur.execute(query)
+  result = cur.fetchall()
+ 
+  cur.close()
+  conn.close()
+  if isPrint:
+    print(result)
+  return result
+
+def GetBidResultsByBizNo(bizNo, isPrint=False):
+  """
+  Parameters:
+  - bizNo(int): 사업자번호
+  Returns:
+  - (id,공고번호,업체명,대표명,사업자번호,등수,입찰액,투찰율,사정율,예가범위,공고일자)
+  """
+  conn = GetConnection()
+  if conn == 0:
+    return
+  cur = conn.cursor()
+  query = f"""
+    SELECT 
+        br.*,
+        b.pricerange,
+        b.biddate
+    FROM 
+        bidresults br
+    JOIN 
+        bids b
+    ON 
+        br.bidno = b.bidno
+    WHERE 
+        br.bizno = '{bizNo}';
+    """
   cur.execute(query)
   result = cur.fetchall()
   
--- KONEPS-Dashboard.py
+++ KONEPS-Dashboard.py
@@ -2,42 +2,49 @@ import Data.data as data_module
 import pandas as pd
 from bokeh.plotting import figure
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, HoverTool, LinearColorMapper, ColorBar, TextInput, Button, DataTable, TableColumn, NumberFormatter, StringFormatter, RadioButtonGroup, Range1d
+from bokeh.models import ColumnDataSource, HoverTool, LinearColorMapper, ColorBar, TextInput, Button, DataTable, TableColumn, NumberFormatter, StringFormatter, DateFormatter, RadioButtonGroup, Range1d, WheelZoomTool, BoxZoomTool, ResetTool
 from bokeh.layouts import column, row
 import numpy as np
 
 def search_callback():
-    input_value = input_field.value
+    input_value = input_field.value.rstrip()
     if not input_value:
         return
     
     selected_option = search_options[radio_button_group.active]
     if selected_option == "공고번호":
+        line_plot.visible = False
         update_barplot(data_module.GetBidResultsByBidNo(input_value))
     elif selected_option == "사업자번호":
-        update_barplot(data_module.GetBizResultsByBizNo(input_value))
-        update_lineplot(data_module.GetBizResultsByBizNo(input_value))
+        line_plot.visible = True
+        update_barplot(data_module.GetBidResultsByBizNo(input_value))
+        update_lineplot(data_module.GetBidResultsByBizNo(input_value))
 
 def update_lineplot(data_list):
     """공고번호에 따라 데이터를 정렬하여 꺾은선 그래프를 그립니다"""
     update_datatable(data_list)
 
-    sorted_data = sorted(data_list, key=lambda x: x[1])  # 공고번호로 정렬
+    sorted_data = sorted(data_list, key=lambda x: x[10])  # 공고일자로 정렬
     
     #line_plot.x_range[sorted_data[1]]
-
-    line_plot.line()
 
     #rank_1_source = ColumnDataSource(sorted_data[sorted_data[5] == 1])
     #line_plot.circle('bidno', 'biddiff', size=8, source=rank_1_source, color='red', legend_label='Bidrank 1', fill_alpha=0.6, line_color=None)
 
     line_plot.y_range = Range1d(start=0.7, end=1.3)
-    
+    print(type(sorted_data[0][10]))
     line_data.data = {
-        'bidno': [item[1] for item in sorted_data],
-        'biddiff': [float(item[8]) for item in sorted_data],
-        'rank': [item[5] for item in sorted_data]
+        'bidNo': [item[1] for item in sorted_data],
+        'bidDiff': [float(item[8]) for item in sorted_data],
+        'rank': [item[5] for item in sorted_data],
+        'bidDate': [pd.to_datetime(item[10]) for item in sorted_data]
     }
+
+    line_plot.renderers.clear()
+
+    # 새로운 라인 추가
+    line_plot.line('bidDate', 'bidDiff', source=line_data, line_width=2, color='blue')
+
 
 def update_barplot(data_list):
     update_datatable(data_list)
@@ -100,60 +107,82 @@ color_mapper = LinearColorMapper(palette="Viridis256")
 
 # Initial plots
 source = ColumnDataSource(data=dict(top=[], left=[], right=[], biz_names=[], line_color=[], line_width=[]))
-bar_plot = figure(title="사정율 히스토그램", tools="", background_fill_color="#fafafa", x_range=(0.96, 1.04))
+bar_plot = figure(title="사정율 구간별 등장 횟수", tools="", background_fill_color="#fafafa", x_range=(0.97, 1.03))
 bar_plot.quad(top='top', bottom=0, left='left', right='right', source=source, fill_color={'field':'top', 'transform': color_mapper}, line_color='line_color', line_width='line_width', alpha=0.7)
+bar_plot.min_border = 75
 
-line_data = ColumnDataSource(data=dict(bidno=[], biddiff=[], rank=[]))
-line_plot = figure(title="사정률 변화 추세", tools="", background_fill_color="#fafafa")
-#line_plot.quad(top='top', bottom=0, left='left', right='right', source=line_data, fill_color={'field':'top', 'transform': color_mapper}, line_color='line_color', line_width='line_width', alpha=0.7)
-line_plot.line('bidno', 'biddiff', source=line_data, line_width=2, color='blue', legend_label='사정율')
+line_data = ColumnDataSource(data=dict(bidNo=[], bidDiff=[], rank=[], bidDate=[]))
+line_plot = figure(title="과거 사정율 사용 기록", tools="", x_axis_type='datetime', background_fill_color="#fafafa")
+line_plot.line('bidDate', 'bidDiff', source=line_data, line_width=2, color='blue')
 
-# Add tooltips
+line_plot.x_range.start = pd.to_datetime('2023-01-01')
+line_plot.x_range.end = pd.to_datetime('2023-12-31')
+line_plot.y_range.start = 0
+line_plot.y_range.end = 10
+
+# Add tools
 bar_hover = HoverTool()
 bar_hover.tooltips = [
-    ("범위", "@left{0.000} - @right{0.000}"),
+    ("범위", "@left{0.0000} - @right{0.0000}"),
     ("값", "@top"),
-    ("업체명", "@biz_names{safe}")
+    ("업체명", """
+        <div style="width: 400px; word-wrap: white-space: normal;">
+            @biz_names{safe}
+        </div>
+    """)
 ]
 bar_plot.add_tools(bar_hover)
 
 line_hover = HoverTool()
 line_hover.tooltips = [
-    ("공고번호", "@bidno"),
-    ("사정율", "@biddiff"),
+    ("공고번호", "@bidNo"),
+    ("사정율", "@bidDiff"),
     ("순위", "@rank")
 ]
+line_hover.attachment = "horizontal"
+
 line_plot.add_tools(line_hover)
+line_plot.add_tools(BoxZoomTool(), ResetTool())
+
+wheel_zoom = WheelZoomTool()
+#bar_plot.add_tools(wheel_zoom)
+#bar_plot.toolbar.active_scroll = wheel_zoom
+#bar_plot.toolbar.active_scroll.axis = 'x'
 
 # DataTable 구성
 columns = [
-    TableColumn(field="공고번호", title="공고번호", formatter=StringFormatter()),
-    TableColumn(field="업체명", title="업체명", formatter=StringFormatter()),
-    TableColumn(field="대표명", title="대표명", formatter=StringFormatter()),
-    TableColumn(field="사업자번호", title="사업자번호", formatter=StringFormatter()),
-    TableColumn(field="순위", title="순위", formatter=StringFormatter()),
-    TableColumn(field="입찰액", title="입찰액", formatter=NumberFormatter(format="0,0")),
-    TableColumn(field="투찰율", title="투찰율", formatter=NumberFormatter(format="0.0000")),
-    TableColumn(field="사정율", title="사정율", formatter=NumberFormatter(format="0.0000")),
+    TableColumn(field="bidNo", title="공고번호", formatter=StringFormatter()),
+    TableColumn(field="bizName", title="업체명", formatter=StringFormatter()),
+    TableColumn(field="bizOwner", title="대표명", formatter=StringFormatter(), width=100),
+    TableColumn(field="bizNo", title="사업자번호", formatter=StringFormatter()),
+    TableColumn(field="rank", title="순위", formatter=StringFormatter(), width=50),
+    TableColumn(field="bidPrice", title="입찰액(원)", formatter=NumberFormatter(format="0,0")),
+    TableColumn(field="bidRatio", title="투찰율", formatter=NumberFormatter(format="0.000"), width=150),
+    TableColumn(field="bidDiff", title="사정율", formatter=NumberFormatter(format="0.0000"), width=150),
+    TableColumn(field="priceRange", title="예가범위", formatter=StringFormatter(), width=100),
+    TableColumn(field="bidDate", title="공고일자", formatter=DateFormatter(format="%Y-%m-%d")),
 ]
 
-table_data = ColumnDataSource(data=dict(공고번호=[], 업체명=[], 대표명=[], 사업자번호=[], 순위=[], 입찰액=[], 투찰율=[], 사정율=[]))
-data_table = DataTable(source=table_data, columns=columns, width=800)
+table_data = ColumnDataSource(data=dict(bidNo=[], bizName=[], bizOwner=[], bizNo=[], rank=[], bidPrice=[], bidRatio=[], bidDiff=[], priceRange=[], bidDate=[]))
+data_table = DataTable(source=table_data, columns=columns, width=900)
 
 def update_datatable(data_list):
     # Update data table
     table_data.data = {
-        '공고번호': [item[1] for item in data_list],
-        '업체명': [item[2] for item in data_list],
-        '대표명': [item[3] for item in data_list],
-        '사업자번호': [item[4] for item in data_list],
-        '순위': [item[5] for item in data_list],
-        '입찰액': [item[6] for item in data_list],
-        '투찰율': [float(item[7]) for item in data_list],
-        '사정율': [float(item[8]) for item in data_list]
+        'bidNo': [item[1] for item in data_list],
+        'bizName': [item[2] for item in data_list],
+        'bizOwner': [item[3] for item in data_list],
+        'bizNo': [item[4] for item in data_list],
+        'rank': [item[5] for item in data_list],
+        'bidPrice': [item[6] for item in data_list],
+        'bidRatio': [float(item[7]) for item in data_list],
+        'bidDiff': [float(item[8]) for item in data_list],
+        'priceRange': [item[9] for item in data_list],
+        'bidDate': [pd.to_datetime(item[10]) for item in data_list],
     }
 
 # Layout
+line_plot.visible = False
 layout = column(radio_button_group, row(input_field, submit_button), row(bar_plot, line_plot), data_table)
 
 # Bokeh 서버에 레이아웃 추가
