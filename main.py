4663b0227380f9091af61159a825987c93cc40c8
웅 장 <ahfrhgkwl@naver.com>
2024-08-30 14:50:59 +0900

    url parameters

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask import request
 from bokeh.embed import server_document
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    script = server_document('http://13.211.37.124:5006/KONEPS-Dashboard')
+    args = request.args
+    script = server_document('http://13.211.37.124:5006/KONEPS-Dashboard', arguments=args)
     return render_template('index.html', script=script)
 
 if __name__ == '__main__':
