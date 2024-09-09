5d91122b996f147494b335f309bdc9f8bf8d4346
웅 장 <ahfrhgkwl@naver.com>
2024-09-10 02:37:14 +0900

    bid analyzer development commit

--- data_client/data.py
+++ data_client/data.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 import datetime
 from datetime import datetime, date
 import logging
@@ -86,6 +87,51 @@ def fetch_bid_by_bid_no(bid_no):
   names = ["bid_no", "is_using_d_value", "bid_min", "base_price", "a_value", "industry", "bid_ord", "is_canceled", "price_range", "d_value", "bid_date", "biz_count"]
   return tuple_to_dict(names, result[0])
 
+def fetch_bids_by_region_and_license(region, license):
+  """
+  Params:
+    region(str): 지역코드
+    industry(str): 업종코드
+  Returns:
+    (공고번호,순공사원가사용여부,입찰하한가,기초가격,A값,자격요건,공고차수,취소여부,예가범위,순공사원가액수,공고일자,참여업체)
+  """
+  query = """
+  SELECT b.*
+  FROM bids b
+  JOIN bidregion br ON b.bidno = br.bidno
+  JOIN bidlicense bl ON b.bidno = bl.bidno
+  WHERE br.region = '%s' AND bl.license = '%s';
+  """
+  params = (region, license)
+  results = fetch_single(query, params)
+  if not results:
+    return None
+  return results
+
+def fetch_bid_regions(bid_no):
+  """
+  Returns:
+    (공고번호,지역)
+  """
+  query = "SELECT * FROM bidregions WHERE bidno = %s"
+  params = (bid_no,)
+  results = fetch_single(query, params)
+  if not results:
+    return None
+  return results
+
+def fetch_bid_licenses(bid_no):
+  """
+  Returns:
+    (공고번호,업종코드,그룹)
+  """
+  query = "SELECT * FROM bidlicense WHERE bidno = %s"
+  params = (bid_no,)
+  results = fetch_single(query, params)
+  if not results:
+    return None
+  return results
+
 def fetch_bid_no_list(bid_no_min, bid_no_max):
   """
   중복 체크를 위해 bid_no 값만 반환
@@ -112,8 +158,6 @@ def fetch_bidresults_by_biz(biz_no):
   """
   Params:
     biz_no(int): 사업자번호
-    date_begin(datetime): 필터링할 날짜 구간의 시작일
-    date_end(datetime): 필터링할 날짜 구간의 종료일
   Returns:
     (id,공고번호,업체명,대표명,사업자번호,순위,입찰액,투찰율,사정율,예가범위,공고일자,참여업체)
   """
@@ -135,6 +179,14 @@ def fetch_bidresults_by_biz(biz_no):
   params = (biz_no,)
   result = fetch_single(query, params)
   return result
+
+def fetch_bidresults_by_bid_list(bid_no_list):
+  """
+  Returns:
+    (id,공고번호,업체명,대표명,사업자번호,순위,입찰액,투찰율,사정율,예가범위,공고일자,참여업체)
+  """
+  query = sql.SQL("SELECT * FROM bidresults WHERE bidno = ANY(%s)")
+  return fetch_single(query, (bid_no_list,))
 
 def filter_bidresults(data_list, date_begin=None, date_end=None, price_range=None, biz_count_min=None, biz_count_max=None):
   if not data_list:
